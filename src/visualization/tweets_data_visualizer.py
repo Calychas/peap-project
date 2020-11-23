44ae4555d4934f1ed8c756eb0b8bed10b5e27f75
Filip Dratwiński <dratwinski.filip@gmail.com>
2020-11-23 19:17:13 +0100

    Add fixes related to the review

--- src/visualization/tweets_data_visualizer.py
+++ src/visualization/tweets_data_visualizer.py
@@ -8,18 +8,22 @@ import sys
 def plot_tweets_counts(accounts: pd.DataFrame) -> None:
     plt.figure(figsize=(12, 12))
     sns.histplot(data=accounts, x="tweets_count", binwidth=500)
-    plt.savefig("reports/plots/tweets_count_hist_full.png")
+    plt.xlabel("Number of tweets for a user")
+    plt.ylabel("Number of accounts")
+    plt.savefig("reports/plots/all_replies_count_hist_full.png")
     plt.close()
 
     accounts_with_less_than_500_tweets = accounts[accounts['tweets_count'] <= 500]
 
     plt.figure(figsize=(12, 12))
     sns.histplot(data=accounts_with_less_than_500_tweets, x="tweets_count", binwidth=20)
-    plt.savefig("reports/plots/tweets_count_less_than_500_hist.png")
+    plt.xlabel("Number of tweets for a user")
+    plt.ylabel("Number of accounts")
+    plt.savefig("reports/plots/all_replies_count_less_than_500_hist.png")
     plt.close()
 
 
 if __name__ == '__main__':
     accounts_processed_file_path = sys.argv[1]
-    accounts_df = pd.read_csv(accounts_processed_file_path, skiprows=[1])
+    accounts_df = pd.read_csv(accounts_processed_file_path)
     plot_tweets_counts(accounts_df)
