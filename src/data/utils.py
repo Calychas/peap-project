4830ff6216dacd57289668cd24aca2608af573fa
Calychas <kacper.lesniara@gmail.com>
2020-12-14 21:58:50 +0100

    add method to load json with comments

--- src/data/utils.py
+++ src/data/utils.py
@@ -2,6 +2,8 @@ from os.path import exists, join
 from os import mkdir, listdir
 from typing import List
 from pathlib import Path
+import regex as re
+import json
 
 
 def maybe_create_dir(dir: str):
@@ -17,3 +19,8 @@ def list_full_paths(dir: str) -> List[str]:
 
 def get_file_name(path: str) -> str:
     return Path(path).stem
+
+
+def jsonc_load(path: str):
+    text = open(path, "r", encoding="utf-8").read()
+    return json.loads(re.sub("//.*", "", text, flags=re.MULTILINE))
