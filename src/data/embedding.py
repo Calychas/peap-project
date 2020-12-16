d2f5da06f92290453c1560b383310e8847ea42ad
Filip Dratwiński <dratwinski.filip@gmail.com>
2020-12-16 01:52:05 +0100

    Modify embedding script so it creates embeddings from pkl.gz file and generates pkl.gz file for each user

--- src/data/embedding.py
+++ src/data/embedding.py
@@ -17,9 +17,9 @@ print(CUDA)
 @click.option("-a", "--aggregation", type=click.Choice(["mean"]), required=True)
 @click.option(
     "-i",
-    "--input-dir",
-    "input_dir",
-    type=click.Path(exists=True, file_okay=False),
+    "--input-tweets-pickle",
+    "tweets_pickle_path",
+    type=click.Path(exists=True),
     required=True,
 )
 @click.option(
@@ -28,10 +28,12 @@ print(CUDA)
 @click.option(
     "-p", "--partial-output-dir", "partial_output_dir", type=click.Path(file_okay=False, exists=False), required=False
 )
-def embed_accounts(model: str, aggregation: str, input_dir: str, output_file: str, partial_output_dir: str):
+def embed_accounts(model: str, aggregation: str, tweets_pickle_path: str, output_file: str, partial_output_dir: str):
     save_partials = partial_output_dir is not None
 
-    if save_partials:
+    tweets_df = pd.read_pickle(tweets_pickle_path)
+
+    if save_partials and not os.path.exists(partial_output_dir):
         os.mkdir(partial_output_dir)
 
     if model == "herbert":
@@ -43,27 +45,21 @@ def embed_accounts(model: str, aggregation: str, input_dir: str, output_file: st
         if aggregation == "mean":
             agg_func = lambda embeddings: np.mean(embeddings, axis=0)
 
-            csv_files = [
-                f
-                for f in os.listdir(input_dir)
-                if os.path.isfile(os.path.join(input_dir, f)) and f.endswith(".csv")
-            ]
+            users = tweets_df['username'].unique()
             results = []
-            i = 0
-            for account_tweets_path in tqdm(csv_files):
-                full_path = os.path.join(input_dir, account_tweets_path)
-                user_tweets = pd.read_csv(full_path)
-                embedding_results = embed_tweets(user_tweets, tokenizer, model, agg_func, save_partials)
-                if save_partials:
-                    username = embedding_results[0]
-                    all_tweets_embeddings = embedding_results[-1]
-                    with open(os.path.join(partial_output_dir, f"{username}.pkl"), 'wb') as f:
-                        pkl.dump(all_tweets_embeddings, f)
+            for user in tqdm(users):
+                if not os.path.exists(os.path.join(partial_output_dir, f"{user}.pkl.gz")):
+                    user_tweets = tweets_df[tweets_df['username'] == user]
+                    embedding_results = embed_tweets(user, user_tweets, tokenizer, model, agg_func, save_partials)
+                    if save_partials:
+                        username = embedding_results[0]
+                        all_tweets_embeddings = embedding_results[-1]
+                        all_tweets_embeddings.to_pickle(os.path.join(partial_output_dir, f"{username}.pkl.gz"))
 
-                    user_embedding = embedding_results[:-1]
-                else:
-                    user_embedding = embedding_results
-                results.append(user_embedding)
+                        user_embedding = embedding_results[:-1]
+                    else:
+                        user_embedding = embedding_results
+                    results.append(user_embedding)
 
             result_df = pd.DataFrame(results, columns=["username", "embedding"])
             result_df.to_csv(output_file, index=False)
@@ -77,13 +73,15 @@ def embed_accounts(model: str, aggregation: str, input_dir: str, output_file: st
 
 
 def embed_tweets(
-    tweets_data: pd.DataFrame, tokenizer, model, aggregation, return_partials=False
+        username: str, tweets_data: pd.DataFrame, tokenizer, model, aggregation, return_partials=False
 ) -> Tuple[str, np.ndarray]:
-    username = tweets_data.at[0, 'username']
+    tweet_indices = tweets_data.index.values.tolist()
+    tweet_ids = tweets_data["id"].tolist()
+    tweet_usernames = tweets_data["username"].tolist()
     tweet_texts = tweets_data["tweet"].tolist()
     tweet_embeddings = []
     with torch.no_grad():
-        for batch in tqdm(chunks(tweet_texts, 150)):
+        for batch in tqdm(chunks(tweet_texts, 150), total=len(tweet_texts) // 150 + 1):
             tokenized_text = tokenizer.batch_encode_plus(
                 batch, padding="longest", add_special_tokens=True, return_tensors="pt"
             )
@@ -93,11 +91,14 @@ def embed_tweets(
             outputs = model(**tokenized_text)
             batch_embeddings = outputs[1].cpu().numpy()
             tweet_embeddings.extend(batch_embeddings)
+
+    data_df = pd.DataFrame(index=tweet_indices, data={"tweet_id": tweet_ids,
+                                                      "username": tweet_usernames,
+                                                      "tweet_embedding": tweet_embeddings})
     all_embeddings = np.vstack(tweet_embeddings)
-    print(all_embeddings.shape)
     account_embedding = aggregation(all_embeddings)
     if return_partials:
-        return username, account_embedding, all_embeddings
+        return username, account_embedding, data_df
     else:
         return username, account_embedding
 
@@ -105,7 +106,7 @@ def embed_tweets(
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
-        yield lst[i : i + n]
+        yield lst[i: i + n]
 
 
 if __name__ == "__main__":
